518c4104d8cecc156b2dc44dca7ffcc817feedd3
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-02-12 03:23:42 +0900

    queue

---
+++ queue_with_node.py
@@ -0,0 +1,61 @@
+class Node(object):
+    def __init__(self, value = None, pointer = None):
+        self.value = value
+        self.pointer = pointer # 새로운 거 저장
+
+class LinkedQueue(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+    
+    def isEmpty(self):
+        return not bool(self.count)
+    
+    def dequeue(self):
+        if self.head:
+            value = self.head.value
+            self.head = self.head.pointer
+            self.count -= 1
+            return value
+        else:
+            return "Queue is empty."
+
+    def enqueue(self, value):
+        node = Node(value)
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            if self.tail:
+                self.tail.pointer = node
+            self.tail = node
+        self.count += 1
+    
+    def size(self):
+        return self.count
+
+    def peek(self):
+        return self.head.value
+
+    def _print(self):
+        node = self.head
+        while node:
+            print(node.value, end=' ')
+            node = node.pointer
+        print()
+
+if __name__ == "__main__":
+    queue = LinkedQueue()
+    print(f"큐가 비었나요? {queue.isEmpty()}")
+    print("스택에 숫자 0~9를 추가합니다.")
+    for i in range(10):
+        queue.enqueue(i)
+    print(f"큐가 비었나요? {queue.isEmpty()}")
+    queue._print()
+
+    print(f"큐 크기: {queue.size()}")
+    print(f"peek: {queue.peek()}")
+    print(f"dequeue: {queue.dequeue()}")
+    print(f"peek: {queue.peek()}")
+    queue._print()
--- stack_with_node.py
+++ stack_with_node.py
@@ -33,7 +33,7 @@ class Stack(object):
     def size(self):
         return self.count
 
-    def _printList(self):
+    def _print(self):
         node = self.head
         while node:
             print(node.value, end=' ')
@@ -46,10 +46,10 @@ if __name__ == "__main__":
     print("스택에 숫자 0~9를 추가합니다.")
     for i in range(10):
         stack.push(i)
-    stack._printList()
+    stack._print()
     print(f"스택 크기: {stack.size()}")
     print(f"peek: {stack.peek()}")
     print(f"pop: {stack.pop()}")
     print(f"peek: {stack.peek()}")
     print(f"스택이 비었나요? {stack.isEmpty()}")
-    stack._printList()
+    stack._print()
