b63d67e8c1f58ee4415f347caa93a1d76135f94e
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-02-13 03:34:37 +0900

    0213

--- max_heap.py
+++ max_heap.py
@@ -8,13 +8,33 @@ class Heapify(object):
         return repr(self.data)
 
     def parent(self, i):
+        if i % 2 == 1:
+            return i // 2
+        else:
+            return i // 2 - 1
 
     def left_child(self, i):
+        return i * 2 + 1
 
     def right_child(self, i):
+        return i * 2 + 2
 
     def __max_heapify__(self, i):
+        largest = i
+        left = self.left_child(i)
+        right = self.right_child(i)
+        n = len(self.data)
 
+        if left < n:
+            if self.data[left] > self.data[i]:
+                largest = left
+        if right < n:
+            if self.data[right] > self.data[largest]:
+                largest = right
+
+        if i != largest:
+            self.data[i], self.data[largest] = self.data[largest], self.data[i]
+            self.__max_heapify__(largest)
     
     def extract_max(self):
         max = self.data[0]
@@ -24,6 +44,12 @@ class Heapify(object):
         return max
     
     def insert(self, item):
+        i = len(self.data)
+        self.data.append(item)
+        while (i != 0) and item > self.data[self.parent(i)]:
+            self.data[i] = self.data[self.parent(i)]
+            i = self.parent(i)
+        self.data[i] = item
 
 def test_heapify():
     l1 = [3, 2, 5, 1, 7, 8, 2]
